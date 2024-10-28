ecd0a2131feb3ea4a35de575f3059e297827c3b0
hhc27 <hhc27@sfu.ca>
2024-10-28 01:29:03 -0700

    Set up basic firebase stuff.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaADublPtWDetKqIjIH_8WwXpYvEyB0bM",
+  authDomain: "closetshare-fb324.firebaseapp.com",
+  projectId: "closetshare-fb324",
+  storageBucket: "closetshare-fb324.appspot.com",
+  messagingSenderId: "167592496969",
+  appId: "1:167592496969:web:57f621fdae721d73351498"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
