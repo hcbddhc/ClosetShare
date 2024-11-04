87bea7e6f8db35a2bbc2aa2e1e1f1f1d3c0cdaab
hhc27 <hhc27@sfu.ca>
2024-11-03 18:36:46 -0800

    First Header Iteration visuals completed

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,6 +1,6 @@
 // src/HomeScreen.js
 import React from 'react';
-import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+import { View, Text, Image, TextInput, ScrollView, Pressable, StyleSheet } from 'react-native';
 import { useFonts, Poppins_700Bold } from '@expo-google-fonts/poppins';
 
 
@@ -18,15 +18,44 @@ const HomeScreen = () => {
   return (
     <View style={styles.container}>
 
-       {/* Header */}
+       {/* ------------------------------------------Header------------------------------------------------------- */}
       <View style= {styles.header}>
+        {/* Logo */}
         <Text style={styles.logo}>CLOSET SHARE.</Text>
-        <View style = {styles.search}>
 
+        {/* Search Bar */}
+        <View style = {styles.search}>
+          <Image style={styles.searchIcon} source={require('../assets/HomeScreenImages/Search Icon.png')} />
           <TextInput style={styles.caption} placeholder="search......"/>
         </View>
-      </View>{/* Header */}
+
+        {/* Filter bar */}
+        <View style = {styles.filter}>
+          <Pressable style={styles.filterIcon}><Image source={require('../assets/HomeScreenImages/Filter Icon.png')}/></Pressable>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.filterOptionsContainer}>
+            <Pressable style={styles.filterOption}>
+              <Text style={styles.filterText}>Gender</Text>
+              <Image source={require('../assets/HomeScreenImages/DropdownIcon.png')} style={styles.dropdownIcon} />
+            </Pressable>
+            <Pressable style={styles.filterOption}>
+              <Text style={styles.filterText}>Height</Text>
+              <Image source={require('../assets/HomeScreenImages/DropdownIcon.png')} style={styles.dropdownIcon} />
+            </Pressable>
+            <Pressable style={styles.filterOption}>
+              <Text style={styles.filterText}>Season</Text>
+              <Image source={require('../assets/HomeScreenImages/DropdownIcon.png')} style={styles.dropdownIcon} />
+            </Pressable>
+            <Pressable style={styles.filterOption}>
+              <Text style={styles.filterText}>Body Type</Text>
+              <Image source={require('../assets/HomeScreenImages/DropdownIcon.png')} style={styles.dropdownIcon} />
+            </Pressable>
+          </ScrollView>
+        </View>
+      </View>
       
+      {/* ------------------------------------------Content------------------------------------------------------- */}
+
+      {/* ------------------------------------------Footer------------------------------------------------------- */}
     </View>
   );
 };
@@ -38,20 +67,53 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
     paddingTop: 56,
     paddingBottom: 10,
+    paddingLeft: 22,
   },
 
   logo: {
     fontFamily: 'Poppins_700Bold',
-    fontSize: 14,
+    fontSize: 20,
     color: '#9D4EDD', 
+    marginBottom: 16,
   },
   search: {
-    marginLeft: 22,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
     marginRight: 22,
     padding: 4,
     backgroundColor: '#F0F0F0',
     borderRadius: 15,
 
+  },
+  searchIcon: {
+    marginRight: 10,
+  },
+  filter :{
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  filterIcon: {
+    marginRight: 10,
+  },
+  filterText: {
+    color: '#666363',
+  },
+  filterOption: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ECCBFF',
+    marginRight: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  dropdownIcon: {
+    height: 15,
+    width: 15,
+    marginLeft: 5,
   }
 });
 
